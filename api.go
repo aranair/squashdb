e6fb55df0e1459590abd843d05d642598164d66f
Homan <boa.homan@gmail.com>
2015-08-06 01:33:59 +0800

    Page show with errors

--- api.go
+++ api.go
@@ -1,31 +1,40 @@
 package main
 
 import (
-	"unicode"
-	"unicode/utf8"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 type __api__ struct{}
 
+type Player struct {
+	Name  string `json:"name"`
+	Grade string `json:"grade"`
+	Club  string `json:"club"`
+}
+
 var api = __api__{}
 
 func (api __api__) bind(r *gin.RouterGroup) {
-	r.GET("/users/:username", api.username)
+	r.GET("/players", api.player_index)
 }
 
-func upperFirst(s string) string {
-	if s == "" {
-		return ""
+func (_ __api__) player_index(c *gin.Context) {
+	// TODO: Query all the players from the db
+	p1 := Player{
+		Name:  "Bobby Pang",
+		Grade: "B2",
+		Club:  "UCSC",
 	}
-	r, n := utf8.DecodeRuneInString(s)
-	return string(unicode.ToUpper(r)) + s[n:]
-}
+	p2 := Player{
+		Name:  "Boa Ho Man",
+		Grade: "B1",
+		Club:  "UCSC",
+	}
+	players := []Player{p1, p2}
 
-func (_ __api__) username(c *gin.Context) {
-	c.JSON(200, map[string]string{
-		"username": c.Params.ByName("username"),
-		"name":     upperFirst(c.Params.ByName("username")),
+	c.JSON(http.StatusOK, gin.H{
+		"players": players,
 	})
 }
--- js/bundle.js
+++ js/bundle.js
@@ -17,64 +17,120 @@ var MainPage = React.createClass({
   render: function() {
     return (
       <div className="MainPage">
-        <h1>Hello, anonymous!</h1>
-        <p><Link href="/users/doe">Login</Link></p>
+        <h1>Hello!</h1>
+        <p><Link href="/players">Players List</Link></p>
       </div>
     );
   }
 });
 
-var UserPage = React.createClass({
+var PlayersPage = React.createClass({
   statics: {
-    getUserInfo: function(username, cb) {
+    getPlayers: function(cb) {
       superagent.get(
-        '/api/v1/users/' + username,
+        '/api/v1/players/',
         function(err, res) {
           cb(err, res ? res.body : null);
         });
     }
   },
-
   getInitialState: function() {
-    var state, username = this.props.username;
-    UserPage.getUserInfo(this.props.username, function(err, res){
-      if (typeof state === 'undefined') {
-        state = res
-      } else {
-        this.setState(res);
-      };
-    }.bind(this));
-    state = state || {
-      username: username,
-      name: username.charAt(0).toUpperCase() + username.slice(1)
-    };
-    return state;
+    return {players: []};
   },
 
-  componentWillReceiveProps: function(nextProps) {
-    if (this.props.username !== nextProps.username) {
-      UserPage.getUserInfo(nextProps.username, function(err, info) {
-        if (err) {
-          throw err;
-        }
-        this.setState(info);
-      }.bind(this));
-    }
+  componentDidMount: function() {
+    PlayersPage.getPlayers(function(err, res) {
+      console.error(err);
+      this.setState({players: res});
+    }.bind(this));
   },
 
   render: function() {
-    var otherUser = this.props.username === 'doe' ? 'ivan' : 'doe';
     return (
-      <div className="UserPage">
-        <h1>Hello, {this.state.name}!</h1>
-        <p>
-          Go to <Link href={"/users/" + otherUser}>/users/{otherUser}</Link>
-        </p>
-        <p><Link href="/">Logout</Link></p>
+      <PlayersList data={this.state.players} />
+    );
+  }
+
+});
+
+var PlayersList = React.createClass({
+  render: function() {
+    var playersList = this.props.data.map(function(p) {
+      return <Player name={p.name} grade={p.grade} club={p.club} />;
+    });
+    return (
+      <div className="players">
+        <h1>Players</h1>
+        <ul className="players-list">
+          {playersList}
+        </ul>
       </div>
     );
   }
 });
+
+var Player = React.createClass({
+  render: function() {
+    return (
+      <div className="player">
+      {this.props.name}
+      {this.props.grade}
+      {this.props.club}
+      </div>
+    );
+  }
+});
+
+// var UserPage = React.createClass({
+//   statics: {
+//     getUserInfo: function(username, cb) {
+//       superagent.get(
+//         '/api/v1/users/' + username,
+//         function(err, res) {
+//           cb(err, res ? res.body : null);
+//         });
+//     }
+//   },
+
+//   getInitialState: function() {
+//     var state, username = this.props.username;
+//     UserPage.getUserInfo(this.props.username, function(err, res){
+//       if (typeof state === 'undefined') {
+//         state = res
+//       } else {
+//         this.setState(res);
+//       };
+//     }.bind(this));
+//     state = state || {
+//       username: username,
+//       name: username.charAt(0).toUpperCase() + username.slice(1)
+//     };
+//     return state;
+//   },
+
+//   componentWillReceiveProps: function(nextProps) {
+//     if (this.props.username !== nextProps.username) {
+//       UserPage.getUserInfo(nextProps.username, function(err, info) {
+//         if (err) {
+//           throw err;
+//         }
+//         this.setState(info);
+//       }.bind(this));
+//     }
+//   },
+
+//   render: function() {
+//     var otherUser = this.props.username === 'doe' ? 'ivan' : 'doe';
+//     return (
+//       <div className="UserPage">
+//         <h1>Hello, {this.state.name}!</h1>
+//         <p>
+//           Go to <Link href={"/users/" + otherUser}>/users/{otherUser}</Link>
+//         </p>
+//       </div>
+//     );
+//   }
+// });
 
 var NotFoundHandler = React.createClass({
 
@@ -82,7 +138,6 @@ var NotFoundHandler = React.createClass({
     return (
       <div>
         <p>Page not found</p>
-        <p><Link href="/">Logout</Link></p>
       </div>
     );
   }
@@ -95,12 +150,12 @@ var App = React.createClass({
       <html>
         <head>
           <link rel="stylesheet" href="/static/css/main.css" />
-          <script src="/static/bundle.js" />
-          <title>Go React Example</title>
+          <script src="/static/js/bundle.js" />
+          <title>SquashDB | Singapore</title>
         </head>
         <Pages className="App" path={this.props.path}>
           <Page path="/" handler={MainPage} />
-          <Page path="/users/:username" handler={UserPage} />
+          <Page path="/players" handler={PlayersPage} />
           <NotFound handler={NotFoundHandler} />
         </Pages>
       </html>
